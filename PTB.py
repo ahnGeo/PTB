5169c5fbf8d2956f171152ab0ddfd4786cf2a441
ahngeo11 <ahngeo11@gmail.com>
2021-08-07 04:54:43 +0900

    Modify block() threshold ver.

--- PTB.py
+++ PTB.py
@@ -44,7 +44,7 @@ class PTB :
 
 
     def threshold(self) :
-        #return : (now)None (maybe later)int
+        #return : (hope)int
 
         import cv2
 
@@ -76,12 +76,12 @@ class PTB :
                 _, img_blue_binary = cv2.threshold(b_img, val, 255, cv2.THRESH_BINARY_INV)
                 img_blue_binary = cv2.bitwise_not(img_blue_binary)
 
-                cv2.imshow('G_window', img_blue_binary)
+                cv2.imshow('B_window', img_blue_binary)
 
-            cv2.namedWindow("Gray_window", cv2.WINDOW_NORMAL)
-            cv2.namedWindow("R_window", cv2.WINDOW_NORMAL)
-            cv2.namedWindow("G_window", cv2.WINDOW_NORMAL)
-            cv2.namedWindow("B_window", cv2.WINDOW_NORMAL)
+            cv2.namedWindow("Gray_window")
+            cv2.namedWindow("R_window")
+            cv2.namedWindow("G_window")
+            cv2.namedWindow("B_window")
 
             cv2.imshow('Gray_window', src)
             cv2.imshow('R_window', src)
@@ -94,10 +94,9 @@ class PTB :
             cv2.createTrackbar('level', 'B_window', 0, 255, on_B)
 
             cv2.waitKey(0)
-            # cv2.destroyAllWindows()
 
 
-    def _hide_highlight(self, img) :
+    def _hide_highlight(self, img, val, val_R, val_G, val_B) :
         #return : opencv img
         
         import cv2
@@ -107,26 +106,54 @@ class PTB :
         b_img, g_img, r_img = cv2.split(img)
 
         #make threshold binary img
-        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        ret, img_multiple_binary = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY_INV)
-        img_multiple_binary = cv2.bitwise_not(img_multiple_binary)
+        if val is not None :
+            img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            ret, img_multiple_binary = cv2.threshold(img_gray, val, 255, cv2.THRESH_BINARY_INV)
+            img_multiple_binary = cv2.bitwise_not(img_multiple_binary)
 
-        img_gray = r_img
-        ret, img_skyblue_binary = cv2.threshold(img_gray, 175, 255, cv2.THRESH_BINARY_INV)
-        img_skyblue_binary = cv2.bitwise_not(img_skyblue_binary)
-            
+        if val_R is not None :
+            _, img_red_binary = cv2.threshold(r_img, val_R, 255, cv2.THRESH_BINARY_INV)
+            img_red_binary = cv2.bitwise_not(img_red_binary)
+
+        if val_G is not None :
+            _, img_green_binary = cv2.threshold(g_img, val_G, 255, cv2.THRESH_BINARY_INV)
+            img_green_binary = cv2.bitwise_not(img_green_binary)
+
+        if val_B is not None :
+            _, img_blue_binary = cv2.threshold(b_img, val_B, 255, cv2.THRESH_BINARY_INV)
+            img_blue_binary = cv2.bitwise_not(img_blue_binary)
+
         #hide highlight compare with binary img
         for y in range(h) :  
             for x in range(w) :
-                if img_multiple_binary[y, x] == 0 or img_skyblue_binary[y, x] == 0 : 
-                    src.itemset(y, x, 0, 0)
-                    src.itemset(y, x, 1, 0)
-                    src.itemset(y, x, 2, 0)
+                if val is not None :
+                    if img_multiple_binary[y, x] == 0 : 
+                        src.itemset(y, x, 0, 0)
+                        src.itemset(y, x, 1, 0)
+                        src.itemset(y, x, 2, 0)
+        
+                if val_R is not None :
+                    if img_red_binary[y, x] == 0 : 
+                        src.itemset(y, x, 0, 0)
+                        src.itemset(y, x, 1, 0)
+                        src.itemset(y, x, 2, 0)
+
+                if val_G is not None :
+                    if img_green_binary[y, x] == 0 : 
+                        src.itemset(y, x, 0, 0)
+                        src.itemset(y, x, 1, 0)
+                        src.itemset(y, x, 2, 0)
+                
+                if val_B is not None :
+                    if img_blue_binary[y, x] == 0 : 
+                        src.itemset(y, x, 0, 0)
+                        src.itemset(y, x, 1, 0)
+                        src.itemset(y, x, 2, 0)
 
         return src
 
 
-    def block(self) :
+    def block(self, val=225, val_R=175, val_G=None, val_B=None) :
         import cv2
         import os
         import numpy as np
@@ -136,23 +163,28 @@ class PTB :
             return
         
         elif type(img_list) != list : # an image
-            src = self._hide_highlight(img_list)
+            src = self._hide_highlight(img_list, val, val_R, val_G, val_B)
             if not os.path.exists(self.fname) :
                 os.makedirs(self.fname)
             cv2.imwrite(self.fname+'/'+self.fname+'.jpg', src)
 
         else :
             for i, img in enumerate(img_list) :
-                src = self._hide_highlight(img)
+                src = self._hide_highlight(img, val, val_R, val_G, val_B)
                 if not os.path.exists(self.fname) :
                     os.makedirs(self.fname)
                 cv2.imwrite(self.fname+'/'+self.fname+'_'+str(i+1)+'.jpg', src)
 
 
-
-#ver1. 굿노트 대표 색상 블락 : block
-#ver2. 자기가 임계점 선택 : threshold, (later)block_ver2
-
+'''
+example
+ 
 ptb = PTB("0003.pdf")
-# ptb.threshold()
-# ptb.block()
+
+#ver1. 굿노트 대표 색상 블락 
+ptb.block()
+
+#ver2. 자기가 임계점 선택
+ptb.threshold()  
+ptb.block(val=225, val_G=226)
+'''
